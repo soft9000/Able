fed2adb65422bc699f0dfb0fcc4ef2155f45048e
Soft9000.com <r.a.nagy@gmail.com>
2018-03-26 04:39:29 -0400

    Related namespace
    C++98

---
+++ Able1/sl_a1net2/build/Debug/GNU-Linux-x86/_ext/1815965308/HttpWebService.o.d
@@ -0,0 +1,164 @@
+build/Debug/GNU-Linux-x86/_ext/1815965308/HttpWebService.o: \
+ ../public/a1net2/HttpWebService.cpp \
+ ../public/a1net2/SimpleSockets/ActiveSocket.h \
+ ../public/a1net2/SimpleSockets/SimpleSocket.h \
+ ../public/a1net2/SocCommon.hpp ../public/a1net2/SimpleSockets/Host.h \
+ ../public/a1net2/SimpleSockets/StatTimer.h \
+ ../public/a1net2/HttpWebService.hpp ../public/a1net2/a1net2.hpp \
+ ../public/Able1.hpp ../public/a1core/stdheader.hpp \
+ ../public/a1core/Array.hpp ../public/a1core/Array_old.hpp \
+ ../public/a1core/ZStr.hpp ../public/a1core/BigFileSize.hpp \
+ ../public/a1core/Random.hpp ../public/a1core/CompArray.hpp \
+ ../public/a1core/StringMatcher.hpp ../public/a1core/ByteBuffer.hpp \
+ ../public/a1core/CommonObject.hpp ../public/a1core/Node.hpp \
+ ../public/a1core/CommonObject.hpp ../public/a1core/FilePath.hpp \
+ ../public/a1core/File.hpp ../public/a1core/Directory.hpp \
+ ../public/a1core/Utilities.hpp ../public/a1core/StdDateTime.hpp \
+ ../public/a1core/TimeFormatter.hpp ../public/a1core/PortTime.hpp \
+ ../public/a1core/NodeWalker.hpp ../public/a1core/NodeFinder.hpp \
+ ../public/a1core/FileSystem.hpp ../public/a1core/ProgramId.hpp \
+ ../public/a1core/RecArray.hpp ../public/a1core/StdSeek.hpp \
+ ../public/a1core/DFOArray.hpp ../public/a1core/ZStr.hpp \
+ ../public/a1core/CompArray.hpp ../public/a1core/File.hpp \
+ ../public/a1core/dfo.hpp ../public/a1core/Platform.hpp \
+ ../public/a1core/Url.hpp ../public/a1core/StdPath.hpp \
+ ../public/a1core/StdOid.hpp ../public/a1core/DelimiterSet.hpp \
+ ../public/a1core/OidMeta.hpp ../public/a1core/TagValue.hpp \
+ ../public/a1core/OidTag.hpp ../public/a1core/OidSender.hpp \
+ ../public/a1core/OidParamQuery.hpp ../public/a1core/OidParamTest.hpp \
+ ../public/a1core/OidList.hpp ../public/a1core/OidController.hpp \
+ ../public/a1core/OidRegistry.hpp ../public/a1core/XmIni.hpp \
+ ../public/a1core/PicketFile.hpp ../public/a1core/DynamicPicketFile.hpp \
+ ../public/a1core/ExeLog.hpp ../public/a1core/JulianLog.hpp \
+ ../public/a1core/ExeLog.hpp ../public/a1core/SimpleTagValue.hpp \
+ ../public/a1core/SimpleTaggedRecord.hpp ../public/a1core/IpcMutex.hpp \
+ ../public/a1net2/URL.hpp ../public/a1net2/SocCommon.hpp \
+ ../public/a1net2/SimpleSockets/SimpleSocket.h
+
+../public/a1net2/SimpleSockets/ActiveSocket.h:
+
+../public/a1net2/SimpleSockets/SimpleSocket.h:
+
+../public/a1net2/SocCommon.hpp:
+
+../public/a1net2/SimpleSockets/Host.h:
+
+../public/a1net2/SimpleSockets/StatTimer.h:
+
+../public/a1net2/HttpWebService.hpp:
+
+../public/a1net2/a1net2.hpp:
+
+../public/Able1.hpp:
+
+../public/a1core/stdheader.hpp:
+
+../public/a1core/Array.hpp:
+
+../public/a1core/Array_old.hpp:
+
+../public/a1core/ZStr.hpp:
+
+../public/a1core/BigFileSize.hpp:
+
+../public/a1core/Random.hpp:
+
+../public/a1core/CompArray.hpp:
+
+../public/a1core/StringMatcher.hpp:
+
+../public/a1core/ByteBuffer.hpp:
+
+../public/a1core/CommonObject.hpp:
+
+../public/a1core/Node.hpp:
+
+../public/a1core/CommonObject.hpp:
+
+../public/a1core/FilePath.hpp:
+
+../public/a1core/File.hpp:
+
+../public/a1core/Directory.hpp:
+
+../public/a1core/Utilities.hpp:
+
+../public/a1core/StdDateTime.hpp:
+
+../public/a1core/TimeFormatter.hpp:
+
+../public/a1core/PortTime.hpp:
+
+../public/a1core/NodeWalker.hpp:
+
+../public/a1core/NodeFinder.hpp:
+
+../public/a1core/FileSystem.hpp:
+
+../public/a1core/ProgramId.hpp:
+
+../public/a1core/RecArray.hpp:
+
+../public/a1core/StdSeek.hpp:
+
+../public/a1core/DFOArray.hpp:
+
+../public/a1core/ZStr.hpp:
+
+../public/a1core/CompArray.hpp:
+
+../public/a1core/File.hpp:
+
+../public/a1core/dfo.hpp:
+
+../public/a1core/Platform.hpp:
+
+../public/a1core/Url.hpp:
+
+../public/a1core/StdPath.hpp:
+
+../public/a1core/StdOid.hpp:
+
+../public/a1core/DelimiterSet.hpp:
+
+../public/a1core/OidMeta.hpp:
+
+../public/a1core/TagValue.hpp:
+
+../public/a1core/OidTag.hpp:
+
+../public/a1core/OidSender.hpp:
+
+../public/a1core/OidParamQuery.hpp:
+
+../public/a1core/OidParamTest.hpp:
+
+../public/a1core/OidList.hpp:
+
+../public/a1core/OidController.hpp:
+
+../public/a1core/OidRegistry.hpp:
+
+../public/a1core/XmIni.hpp:
+
+../public/a1core/PicketFile.hpp:
+
+../public/a1core/DynamicPicketFile.hpp:
+
+../public/a1core/ExeLog.hpp:
+
+../public/a1core/JulianLog.hpp:
+
+../public/a1core/ExeLog.hpp:
+
+../public/a1core/SimpleTagValue.hpp:
+
+../public/a1core/SimpleTaggedRecord.hpp:
+
+../public/a1core/IpcMutex.hpp:
+
+../public/a1net2/URL.hpp:
+
+../public/a1net2/SocCommon.hpp:
+
+../public/a1net2/SimpleSockets/SimpleSocket.h:
---
+++ Able1/sl_a1net2/build/Debug/GNU-Linux-x86/_ext/929555163/PassiveSocket.o.d
@@ -0,0 +1,19 @@
+build/Debug/GNU-Linux-x86/_ext/929555163/PassiveSocket.o: \
+ ../public/a1net2/SimpleSockets/PassiveSocket.cpp \
+ ../public/a1net2/SimpleSockets/PassiveSocket.h \
+ ../public/a1net2/SimpleSockets/ActiveSocket.h \
+ ../public/a1net2/SimpleSockets/SimpleSocket.h \
+ ../public/a1net2/SocCommon.hpp ../public/a1net2/SimpleSockets/Host.h \
+ ../public/a1net2/SimpleSockets/StatTimer.h
+
+../public/a1net2/SimpleSockets/PassiveSocket.h:
+
+../public/a1net2/SimpleSockets/ActiveSocket.h:
+
+../public/a1net2/SimpleSockets/SimpleSocket.h:
+
+../public/a1net2/SocCommon.hpp:
+
+../public/a1net2/SimpleSockets/Host.h:
+
+../public/a1net2/SimpleSockets/StatTimer.h:
